8960f96b11cf1ed2500a21451dece250d7e272d6
Ganeshgandhala <85453354+Ganeshgandhala@users.noreply.github.com>
2021-06-06 23:19:01 +0530

    Add files via upload

---
+++ Flask/ccpp.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+
+app=Flask(__name__) #your application
+lr=pickle.load(open('ccpp.pkl','rb'))
+
+
+@app.route('/') # default route
+def home():
+    return render_template("ccpp.html")
+
+
+@app.route('/predict',methods=['post'])
+def predict():
+    AT=float(request.form['AT'])
+    AP=float(request.form['AP'])
+    RH=float(request.form['RH'])
+    PE=float(request.form['PE'])
+    
+    print(AT,AP,RH,PE)
+    a=np.array([[AT,AP,RH,PE]])
+    
+    result=lr.predict(a)
+    
+    return render_template('ccpp.html',x=result)
+
+if __name__ == '__main__':
+    app.run(port=8000) # you are running your app
